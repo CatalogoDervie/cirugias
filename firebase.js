2f336b1853cb8c83628249a73bd7a3dd0e2b6c9d
CatalogoDervie <xdjeree@gmail.com>
2026-03-11 18:13:51 -0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,39 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+  orderBy,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDf49KWGdZp1C3t42LSopS_VuNiaabFZQw",
+  authDomain: "control-cirugias-c9c71.firebaseapp.com",
+  projectId: "control-cirugias-c9c71",
+  storageBucket: "control-cirugias-c9c71.firebasestorage.app",
+  messagingSenderId: "714322418539",
+  appId: "1:714322418539:web:523a8e5ad295d277b20a2e",
+  measurementId: "G-T7SBGEQ2G9"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {
+  db,
+  collection,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+  orderBy,
+  serverTimestamp
+};
